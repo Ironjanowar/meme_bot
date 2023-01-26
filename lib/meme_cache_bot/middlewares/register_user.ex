c4e115607c1fb682e4564df575fd7d4d7cc48791
Cesar Guzman <cesar.guzman@meetsam.io>
2023-01-26 19:32:25 +0100

    Don't crash if user can't be extracted

--- lib/meme_cache_bot/middlewares/register_user.ex
+++ lib/meme_cache_bot/middlewares/register_user.ex
@@ -7,16 +7,20 @@ defmodule MemeCacheBot.Middlewares.RegisterUser do
   import ExGram.Dsl
 
   def call(%Cnt{update: update} = cnt, _opts) do
-    {:ok, %{id: telegram_id} = user} = extract_user(update)
-
-    if UserStore.find_user(telegram_id: telegram_id) == nil do
-      user
-      |> Map.put(:telegram_id, telegram_id)
-      |> UserStore.insert_user()
+    with {:ok, %{id: telegram_id} = user} <- extract_user(update) do
+      maybe_register_user(telegram_id, user)
     end
 
     cnt
   end
 
   def call(cnt, _), do: cnt
+
+  defp maybe_register_user(telegram_id, user) do
+    if UserStore.find_user(telegram_id: telegram_id) == nil do
+      user
+      |> Map.put(:telegram_id, telegram_id)
+      |> UserStore.insert_user()
+    end
+  end
 end
